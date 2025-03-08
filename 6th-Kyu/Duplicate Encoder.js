a0ac86e86a836cd0dfecfe4efef1d6714eb24874
ebubecodes <ebube17@gmail.com>
2025-03-09 00:35:42 +0100

    done*

---
+++ 6th-Kyu/Duplicate Encoder.js
@@ -0,0 +1,27 @@
+/*
+he goal of this exercise is to convert a string to a new string where each character in the newstring
+is "(" if that character appears only once in the original string, or ")" if that character appears
+more than once in the original string. Ignore capitalization when determining if a character is a
+duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should
+encode XXX", the "XXX" is the expected result, not the input!
+
+StringsArraysFundamentals
+*/
+
+function duplicateEncode(word){
+    return word
+      .toLowerCase()
+      .split('')
+      .map( function (a, i, w) {
+        return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+      })
+      .join('');
+};
